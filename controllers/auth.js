0d534e3cac549f5af7012802c58d998fc517840a
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-08 16:30:16 +0100

    add controolers register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,23 @@
-exports.register = (req,res,next)=>{
-    res.send("register route")
+const User = require('../models/User')
+exports.register = async(req,res,next)=>{
+    const {username , email , password }= req.body;
+    try {
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+        res.status(201).json({
+            succes: true,
+            user
+        })
+    } catch (error) {
+        res.status(500).json({
+            succes:false,
+            error:error.message
+        })
+    }
+
 };
 exports.login = (req,res,next)=>{
     res.send("login route")
