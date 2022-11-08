80f382076f0eed4d4e9f0ecfeddd0386a34f1a56
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-08 15:44:05 +0100

    connacxion avac la base de donnée

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connectDb = async()=>{
+    await mongoose.connect(process.env.URI,
+    {   
+        useNewUrlParser:true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+    })
+    console.log("db connected")
+}
+
+
+module.exports=connectDb;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const app = express()
+const connectDb = require('./config/db')
+
+//connect db 
+connectDb();
 
 app.use(express.json())
 app.use("/api/auth",require('./routes/auth'))
