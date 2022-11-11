8837746d7903664f4339dd016221d57a399b2f3f
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-11 16:15:39 +0100

    Creating Private Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import {BroserRouter as Router,Switch,Route} from 'react-router-dom'
+//Route
+import PrivateRoute from './components/routing/PrivateRoute';
+//screen
+import PrivateScreen from './components/screens/PrivateScreen'
+import LoginScreen from './components/screens/LoginScreen'
+import RegisterScreen from './components/screens/RegisterScreen'
+import ForgotpasswordScreen from './components/screens/forgotpasswordScreen'
+import ResetpasswordScreen from './components/screens/resetpasswordScreen'
 
 const App=()=>{
   return (
     <Router>
     <div className="App">
       <Switch>
-
+    < exact path='/' component={PrivateScreen}/>
     <Route  exact path='/login' component={LoginScreen}/>
-    <Route  exact path='/register' component={registerScreen}/>
-    <Route  exact path='/forgotpassword' component={forgotpasswordScreen}/>
-    <Route  exact path='passwordresset/:resetToken' component={resetpasswordScreen}/>
+    <Route  exact path='/register' component={RegisterScreen}/>
+    <Route  exact path='/forgotpassword' component={ForgotpasswordScreen}/>
+    <Route  exact path='passwordresset/:resetToken' component={ResetpasswordScreen}/>
 
       </Switch>
      React app 
